42fd6e894001dc3164167d8859139168a149c5e0
Anon A Mouse <lionsmane@protonmail.com>
2026-02-03 12:25:34 -0600

    Add All Minnesota logo to main site and personalize Django admin (branding, green/gold theme)

--- allminnesota_project/urls.py
+++ allminnesota_project/urls.py
@@ -8,6 +8,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# Personalize Django admin for All Minnesota
+admin.site.site_header = 'All Minnesota Administration'
+admin.site.site_title = 'All Minnesota Admin'
+admin.site.index_title = 'Site administration'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
